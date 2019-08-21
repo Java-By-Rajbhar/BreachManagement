5d3b55f09f2978eef290ec5ee5d015da4c26bbd0
user1 <anuradha.krishnanov7@gmail.com>
2019-08-21 13:02:47 +0530

    initial project

---
+++ src/main/java/com/maggie/app/MaggieBreachManagementApplication.java
@@ -0,0 +1,13 @@
+package com.maggie.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaggieBreachManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaggieBreachManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maggie/app/MaggieBreachManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maggie.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaggieBreachManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
